b536d02e486a41f8e278f97ebc2e70bb3b8df77a
Marti Municoy <martimunicoy@gmail.com>
2017-11-29 18:39:22 +0100

    New assignment folder is added to the repository corresponding to the MPI assignment

---
+++ MPI/args_parser.h
@@ -0,0 +1,74 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+#define ARGS_NUM 5
+
+const int DEFAULT_N = 4096;
+const int DEFAULT_ITER_MAX = 1000;
+const bool DEFAULT_OUT = false;
+const char DEFAULT_FILE_DIR[50] = "lapFusion_results.tsv";
+const bool DEFAULT_COUNT = false;
+const char ARGS[ARGS_NUM][10] = {"-N", "-I", "-O", "-F", "-C"};
+
+struct Args{
+  int n;
+  int iter_max;
+  bool out;
+  char *file_dir;
+  bool count;
+};
+
+bool startsWith(const char *string, const char *prefix){
+  return strncmp(string, prefix, strlen(prefix)) == 0;
+}
+
+bool endsWith(const char *string, const char *sufix){
+  string = strrchr(string, '.');
+  if (string != NULL)
+    return strncmp(string, sufix, strlen(sufix)) == 0;
+  return false;
+}
+
+struct Args args_parser(int argc, char *argv[])
+{
+  int n = DEFAULT_N;
+  int iter_max = DEFAULT_ITER_MAX;
+  bool out = DEFAULT_OUT;
+  char *file_dir = malloc(50);
+  strcpy(file_dir, DEFAULT_FILE_DIR);
+  bool count = DEFAULT_COUNT;
+
+  for (int i = 1; i < argc; i++){
+    for (int j = 0; j < ARGS_NUM; j++){
+      if (startsWith(*(argv + i), ARGS[j])){
+        if (j == 2) out = true;
+        else if (j == 4) count = true;
+        else if (i + 2 <= argc)
+        {
+          if (j == 0) n = atoi(*(argv + i + 1));
+          else if (j == 1) iter_max = atoi(*(argv + i + 1));
+          else if (j == 3) strcpy(file_dir, *(argv + i + 1));
+        }
+        else{
+          printf("Wrong command, using default values.\n");
+        }
+        break;
+      }
+    }
+  }
+  if (n<1 | n>8192){
+    printf("\'N\' out of range, using default value (%d)\n", DEFAULT_N);
+    n = DEFAULT_N;
+  }
+  if (iter_max<1 | iter_max>999999){
+    printf("\'iter_max\' out of range, using default value (%d)\n", DEFAULT_ITER_MAX);
+    iter_max = DEFAULT_ITER_MAX;
+  }
+  if (!endsWith(file_dir, ".tsv")){
+    printf("Wrong \'file_dir\', using default name (%s)\n", DEFAULT_FILE_DIR);
+    strcpy(file_dir, DEFAULT_FILE_DIR);
+  }
+  struct Args parsed_args = {n, iter_max, out, file_dir, count};
+  return parsed_args;
+}
---
+++ MPI/lapFusion_v1.c
@@ -0,0 +1,149 @@
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include "args_parser.h"
+#include <mpi.h>
+
+float stencil ( float v1, float v2, float v3, float v4)
+{
+  return (v1 + v2 + v3 + v4) * 0.25f;
+}
+
+float max_error ( float prev_error, float old, float new )
+{
+  float t= fabsf( new - old );
+  return t>prev_error? t: prev_error;
+}
+
+float laplace_step(float *in, float *out, int n, int rank, int size)
+{
+  int i, j;
+  float error=0.0f;
+  for (j = n*rank/size; j < n*(rank + 1)/size; j++)
+    for (i=0; i < n; i++)
+    {
+      out[j*n+i]= stencil(in[j*n+i+1], in[j*n+i-1], in[(j-1)*n+i], in[(j+1)*n+i]);
+      error = max_error( error, out[j*n+i], in[j*n+i] );
+    }
+  return error;
+}
+
+void laplace_init(float *in, int ri, int rf, int n)
+{
+  int i;
+  float V;
+  const float pi  = 2.0f * asinf(1.0f);
+  printf("%d, %d\n", ri, rf);
+  memset(in, 0, n*(rf-ri+2)*sizeof(float));
+  for (i = ri; i < rf; i++) {
+    V = in[i*n] = sinf(pi*i / (n-1));
+    in[i*n+n-1] = V * expf(-pi);
+  }
+}
+
+void print_matrix(float *matrix, int n, char file_dir[50])
+{
+  int i, j;
+  FILE *f = fopen(file_dir, "w");
+
+  if (f == NULL)
+  {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+
+  for(i = 0; i < n; i++)
+  {
+    for(j = 0; j < n; j++) fprintf(f, "%f\t", matrix[i*n+j]);
+    fprintf(f, "\n");
+  }
+
+  fclose(f);
+}
+
+int main(int argc, char** argv)
+{
+  float *A, *temp;
+  struct timeval t0, t1;
+
+  // get runtime arguments
+  struct Args parsed_args = args_parser(argc, argv);
+  int n = parsed_args.n;
+  int iter_max = parsed_args.iter_max;
+  bool out = parsed_args.out;
+  char *file_dir = malloc(50);
+  strcpy(file_dir, parsed_args.file_dir);
+  bool count = parsed_args.count;
+
+  if (count) gettimeofday(&t0, 0);
+
+  const float tol = 1.0e-5f;
+  float error= 1.0f;
+
+  int iter = 0;
+
+  // Initiate MPI region
+  int rank, size;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  // Determine initial and final rows indexes
+  int ri = n*rank/size;
+  int rf = n*(rank+1)/size;
+
+  // Allocate memory to matrices
+  A    = (float*) malloc( n*n*sizeof(float)/size+2*n );
+  temp = (float*) malloc( n*n*sizeof(float)/size+2*n );
+
+  // Set boundary conditions
+  laplace_init (A, ri, rf, n);
+  laplace_init (temp, ri, rf, n);
+
+  //print_matrix(A, n*n/size+2, file_dir);
+
+  /*
+  // set singular point
+  A[(n/128)*n+n/128] = 1.0f;
+
+  if (rank == 0)
+    printf("Jacobi relaxation Calculation: %d x %d mesh,"
+         " maximum of %d iterations, using %d processors\n",
+         n, n, iter_max, size);
+
+
+  while ( error > tol*tol && iter < iter_max )
+  {
+    iter++;
+    error= laplace_step (A, temp, n, rank, size);
+
+    // swap pointers A & temp
+    float *swap= A; A=temp; temp= swap;
+  }
+  */
+
+  // End MPI region
+  MPI_Finalize();
+
+  /*
+  error = sqrtf( error );
+
+  if (out)
+    print_matrix(A, n, file_dir);
+
+  printf("Total Iterations: %5d, ERROR: %0.6f, ", iter, error);
+  printf("A[%d][%d]= %0.6f", n/128, n/128, A[(n/128)*n+n/128]);
+
+  if (count)
+  {
+    gettimeofday(&t1, 0);
+    long int diff = (t1.tv_sec - t0.tv_sec) *1000000L + t1.tv_usec - t0.tv_usec;
+    printf(", Running time: %ld", diff);
+  }
+  printf("\n");
+  */
+  free(file_dir);
+  free(A); free(temp);
+}
